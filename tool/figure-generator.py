108a472bdd291cd50705ece8ec1c9d2c9afba0e7
Jason Wang <wangborong@cdjrlc.com>
2024-09-13 17:04:08 +0800

    Improve figure-generator engine

--- tool/figure-generator.py
+++ tool/figure-generator.py
@@ -9,22 +9,53 @@ import base64
 import requests
 import argparse
 import subprocess
-from multiprocessing import Pool
-
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-
-def shelldo(cmd):
-    subprocess.run(cmd, shell=True, check=True)
-
-def check_file_existence(file_path):
-    return os.path.exists(file_path) and os.path.isfile(file_path)
+from multiprocessing import Pool, Process
 
 
-class Drawio():
+class FigEngine:
     def __init__(self, appath) -> None:
         self.appath = appath
+        self._types = []
 
+    def eprint(self, *args, **kwargs):
+        print(*args, file=sys.stderr, **kwargs)
+
+    def shelldo(self, cmd):
+        subprocess.run(cmd, shell=True, check=True)
+
+    def check_file_existence(self, file_path):
+        return os.path.exists(file_path) and os.path.isfile(file_path)
+
+    def _get_cmd(self, *args):
+        return f'echo FigEngine: {args}'
+
+    def conv_single_to(self, file,  target, type='pdf', force=False):
+        figfn = re.sub(r'\.\w+$', r'', os.path.basename(file))
+        figpath = os.path.dirname(file)
+        target = target if target else figpath + '/figure'
+        if not os.path.exists(target):
+            os.mkdir(target)
+
+        if self._types and type not in self._types:
+            self.eprint(f'Error: {self.appath} can not convert to {type}')
+            exit(1)
+
+        fig_path = '{}/{}.{}'.format(target, figfn, type)
+        if force or not self.check_file_existence(fig_path):
+            cmd = self._get_cmd(self.appath, type, fig_path, file)
+            try:
+                self.shelldo(cmd)
+            except:
+                self.eprint(f'Error: {self.appath} execute failed')
+
+    def conv_to(self, files, output, type='pdf', force=False):
+        args = [(file, output, type, force) for file in files]
+        with Pool(processes=8) as p:
+            rs = [p.apply_async(self.conv_single_to, args=_args) for _args in args]
+            for r in rs:
+                r.get()
+
+class Drawio(FigEngine):
     def _get_diagram_names(self, diofile):
         diagnames = []
         diagname_re = re.compile(r' *<diagram.*name=\"([\w-]*)\".*>')
@@ -45,111 +76,82 @@ class Drawio():
         return diagnames
 
     def _convert(self, args):
+        if len(args) == 0:
+            return
         cmd = f'{args[0]} --export --format {args[1]} --page-index {args[2]} ' \
             f'{args[3]} --output {args[4]} {args[5]} >/dev/null 2>&1'
         try:
             subprocess.run(cmd, shell=True, check=True)
             print(f'{args[5]} [P{args[2]}] -> {args[4]}')
         except:
-            eprint(f'Convert {args[5]} [P{args[2]}] failed')
+            self.eprint(f'Error: Convert {args[5]} [P{args[2]}] failed')
 
-    def conv_to(self, diofile,  tgtpath, type='pdf', force=False):
-        diofn = os.path.basename(diofile).replace('.drawio', '')
-        diopath = os.path.dirname(diofile)
-        tgtpath = tgtpath if tgtpath else diopath + '/figure'
-        if not os.path.exists(tgtpath):
-            os.mkdir(tgtpath)
+    def conv_single_to(self, file,  target, type='pdf', force=False):
+        diofn = os.path.basename(file).replace('.drawio', '')
+        diopath = os.path.dirname(file)
+        target = target if target else diopath + '/figure'
+        if not os.path.exists(target):
+            os.mkdir(target)
         _types = ["pdf", "png", "jpg", "svg", "vsdx", "xml"]
         if type not in _types:
-            print('drawio can not convert to {}'.format(type))
+            self.eprint('Error: drawio can not convert to {}'.format(type))
             exit(1)
-        diagnames = self._get_diagram_names(diofile)
+        diagnames = self._get_diagram_names(file)
         addopt = '--crop' if type == 'pdf' else ''
 
-        fig_paths = [f'{tgtpath}/{diofn}{diagname}.{type}' for diagname in diagnames]
-        cmds = [[self.appath, type, i, addopt, fig_paths[i], diofile] if not
-            check_file_existence(fig_paths[i]) or force else [] for i in range(0, len(diagnames))]
-        cmdslice = [cmds[i:i + 8] for i in range(0, len(cmds), 8)]
+        fig_paths = [f'{target}/{diofn}{diagname}.{type}' for diagname in diagnames]
+        cmds = [[self.appath, type, i, addopt, fig_paths[i], file] if not
+            self.check_file_existence(fig_paths[i]) or force else [] for i in range(0, len(diagnames))]
 
         with Pool(processes=8) as p:
-            for cmdpart in cmdslice:
-                result = p.map_async(self._convert, cmdpart)
-                result.get(timeout=3)
+            result = p.map_async(self._convert, cmds)
+            result.wait()
+
+    def conv_to(self, files, output, type='pdf', force=False):
+        args = [(file, output, type, force) for file in files]
+        ps = [Process(target=self.conv_single_to, args=_args) for _args in args]
+        for p in ps:
+            p.start()
+        for p in ps:
+            p.join()
 
 
-class Gradot():
+class Gradot(FigEngine):
     def __init__(self, appath) -> None:
-        self.appath = appath
+        super().__init__(appath)
+        self._types = ["pdf", "ps", "png", "svg", "fig",
+                       "gif", "jpg", "jpeg", "json"]
 
-    def conv_to(self, dotfile,  tgtpath, type='pdf', force=False):
-        dotfn = os.path.basename(dotfile).replace('.dot', '')
-        dotpath = os.path.dirname(dotfile)
-        tgtpath = tgtpath if tgtpath else dotpath + '/figure'
-        if not os.path.exists(tgtpath):
-            os.mkdir(tgtpath)
-        _types = ["pdf", "ps", "png", "svg", "fig",
-                  "gif", "jpg", "jpeg", "json"]
-        if type not in _types:
-            print('gradot can not convert to {}'.format(type))
-            exit(1)
-
-        fig_path = '{}/{}.{}'.format(tgtpath, dotfn, type)
-        if force or not check_file_existence(fig_path):
-            gradot_conv_cmd = '{} -T{} ' \
-                '-o {} {}'.format(self.appath, type, fig_path, dotfile)
-            shelldo(gradot_conv_cmd)
+    def _get_cmd(self, *args):
+        return '{} -T{} -o {} {}'.format(*args)
 
 
-class Inkcvt():
+class Inkcvt(FigEngine):
     def __init__(self, appath) -> None:
-        self.appath = appath
+        super().__init__(appath)
+        self._types = ["eps", "emf", "wmf", "xaml", "pdf", "ps", "png", "svg"]
 
-    def conv_to(self, figfile,  tgtpath, type='pdf', force=False):
-        figfn = re.sub(r'\.\w+$', r'', os.path.basename(figfile))
-        figpath = os.path.dirname(figfile)
-        tgtpath = tgtpath if tgtpath else figpath + '/figure'
-        if not os.path.exists(tgtpath):
-            os.mkdir(tgtpath)
-        _types = ["eps", "emf", "wmf", "xaml", "pdf", "ps", "png", "svg"]
-        if type not in _types:
-            print('inkscape can not convert to {}'.format(type))
-            exit(1)
-
-        fig_path = '{}/{}.{}'.format(tgtpath, figfn, type)
-        if force or not check_file_existence(fig_path):
-            inkscape_conv_cmd = '{} --export-type={} ' \
-                '-o {} {}'.format(self.appath, type, fig_path, figfile)
-            shelldo(inkscape_conv_cmd)
+    def _get_cmd(self, *args):
+        return '{} --export-type={} -o {} {}'.format(*args)
 
 
-class Immcvt():
+
+class Immcvt(FigEngine):
     def __init__(self, appath) -> None:
-        self.appath = appath
-
-    def conv_to(self, figfile,  tgtpath, type='pdf', force=False):
-        figfn = re.sub(r'\.\w+$', r'', os.path.basename(figfile))
-        figpath = os.path.dirname(figfile)
-        tgtpath = tgtpath if tgtpath else figpath + '/figure'
-        if not os.path.exists(tgtpath):
-            os.mkdir(tgtpath)
-
+        super().__init__(appath)
         # ImageMagick supported image formats over 200, no need
         # to check the file's type.
 
-        fig_path = '{}/{}.{}'.format(tgtpath, figfn, type)
-        if force or not check_file_existence(fig_path):
-            convert_cmd = '{} {} {}'.\
-                format(self.appath, figfile, fig_path)
-            shelldo(convert_cmd)
+    def _get_cmd(self, *args):
+        return '{} {} {}'.format(args[0], args[3], args[2])
 
 
-class Kroki():
+class Kroki(FigEngine):
     def __init__(self) -> None:
         self._tool_box = {'.mmd': 'mermaid', '.dot': 'graphviz',
                           '.puml': 'plantuml'}
-        pass
 
-    def conv_to(self, file,  tgtpath, type, force=False):
+    def conv_single_to(self, file, target, type='svg', force=False):
         _fname = os.path.basename(file)
         (fname, fsufx) = os.path.splitext(_fname)
         with open(file, 'r') as fr:
@@ -163,14 +165,16 @@ class Kroki():
             reqstr
         )
 
-        fig_path = '{}/{}.{}'.format(tgtpath, fname, type)
-        if force or not check_file_existence(fig_path):
+        if not os.path.exists(target):
+            os.mkdir(target)
+        fig_path = '{}/{}.{}'.format(target, fname, type)
+        if force or not self.check_file_existence(fig_path):
             response = requests.get(kroki_url)
             if response.status_code == 200:
                 with open(fig_path, "wb") as fw:
                     fw.write(response.content)
             else:
-                print('remote response error')
+                self.eprint('Error: Remote response error')
 
 
 def cli():
@@ -185,7 +189,7 @@ def cli():
                         default='/usr/bin/inkscape',
                         help='the inkscape path')
     parser.add_argument('-m', '--imagemagick', type=str,
-                        default='/usr/bin/convert',
+                        default='/usr/bin/magick',
                         help='the imagemagick path')
     parser.add_argument('-c', '--convert', action="store_true",
                         help='use inkscape or imagemagick convertor')
@@ -206,22 +210,49 @@ def cli():
 if __name__ == "__main__":
     args = cli()
 
-    dio = Drawio(args.drawio)
-    dot = Gradot(args.gradot)
-    kroki = Kroki()
-    ink = Inkcvt(args.inkscape)
-    imm = Immcvt(args.imagemagick)
+    # drawio
+    drawios = []
+    dotfiles = []
+    mmds = []
+    figs = []
+
+    filter_re = re.compile(r'.*[.](\w+)$')
     for file in args.source:
-        _, fext = os.path.splitext(file)
-        if fext == '.drawio':
-            dio.conv_to(file, args.output, args.format, args.force)
-        elif fext == '.dot':
-            dot.conv_to(file, args.output, args.format, args.force)
-        elif fext == '.mmd':
-            kroki.conv_to(file, args.output, args.format, args.force)
+        if not os.path.exists(file):
+            print(f'No such file: {file}, ignore it')
+            continue
+        m = filter_re.search(file)
+        if not m:
+            print(f'figure-generator can not get {file} type, ignore it')
+            continue
+        ext = m.group(1)
+        if ext == 'drawio':
+            drawios.append(file)
+        elif ext == 'dot':
+            dotfiles.append(file)
+        elif ext == 'mmd':
+            mmds.append(file)
         else:
-            if args.convert and fext == '.svg':
-                ink.conv_to(file, args.output, args.format, args.force)
+            figs.append(file)
+
+    if figs:
+        if not args.convert:
+            print(f'Specify --convert option when convert figures')
+            exit(1)
+        else:
+            svgs = [f for f in figs if '.svg' in f]
+            others = [f for f in figs if '.svg' not in f]
+            if svgs:
+                Inkcvt(args.inkscape).conv_to(svgs, args.output, args.format, args.force)
             else:
-                imm.conv_to(file, args.output, args.format, args.force)
+                Immcvt(args.imagemagick).conv_to(others, args.output, args.format, args.force)
+
+    if drawios:
+        Drawio(args.drawio).conv_to(drawios, args.output, args.format, args.force)
+            
+    if dotfiles:
+        Gradot(args.gradot).conv_to(dotfiles, args.output, args.format, args.force)
+
+    if mmds:
+        Kroki().conv_to(mmds, args.output, args.format, args.force)
 
